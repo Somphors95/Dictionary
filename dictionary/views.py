d0b2353a07a41091229af21cae13e5063f4a73ba
Som phors <somphorstae9@gmail.com>
2024-03-22 01:51:34 +0700

    already

---
+++ dictionary/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from PyDictionary import PyDictionary
+
+def home(request):
+    return render(request, 'home.html')
+
+def word(request):
+    search = request.GET.get('search')
+    dictionay = PyDictionary()
+    meaning = dictionay.meaning(search)
+    antonym = dictionay.antonym(search)
+    synonym = dictionay.synonym(search)
+
+    context = {
+        'meaning': meaning['Noun'][0],
+        'antonym': antonym,
+        'synonym': synonym,
+    }
+    return render(request, 'word.html', context)
